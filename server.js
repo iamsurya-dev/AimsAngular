c3689cd1e3d322b5958fd1638b4dc7c8920226e3
Suryaprakash Vijayaraghavan <suryaprakash@outlook.com>
2015-11-17 20:48:46 -0800

    Initial commits for Angular Tutorial

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+//var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname));
+//app.use(bodyParser.json());
+
+app.listen(3000);
+console.log("server running on port 3000");
